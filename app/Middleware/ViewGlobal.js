235f2632785429b760af7dd9396778061e4fc943
Mike Conrad <mike@enxo.co>
2020-02-07 21:01:30 -0500

    Made improvements to SEO.  Fixed bug with Github links

--- app/Middleware/ViewGlobal.js
+++ app/Middleware/ViewGlobal.js
@@ -2,6 +2,7 @@
 const edge = require('edge.js')
 const moment = require('moment')
 const Env = use('Env')
+const View = use('View')
 
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
@@ -14,10 +15,13 @@ class ViewGlobal {
    * @param {Request} ctx.request
    * @param {Function} next
    */
-  async handle ({ request }, next) {
+  async handle ({ request, view }, next) {
     // call next to advance the request
     edge.global('currentYear', () => moment().format('YYYY'))
-    edge.global('gitRepo', () => Env.get('GIT_REPO'))
+    view.share({
+      gitRepo: Env.get('GIT_REPO')
+    })
+    // edge.global('gitRepo', () => Env.get('GIT_REPO'))
     await next()
     
   }
